b639e64f99a80e137a9c0247f89b68c6e1c00482
Kevin Noll <kevinnoll90@gmail.com>
2017-02-26 15:33:06 +0100

    made split panel work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Nav } from 'ionic-angular';
+import { SelectPage } from '../select/select';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Nav) nav: Nav;
+  selectPage = SelectPage;
 
   constructor(public navCtrl: NavController) {
+  }
+
+  openPage (sMap) {
     
+    this.nav.setRoot(SelectPage);
+    /* else {
+      this.nav.setRoot(page.component).catch(() => {
+        console.log("Didn't set nav root");
+      });
+    }*/
+
+    /*if (page.logsOut === true) {
+      // Give the menu time to close before changing to logged out
+      setTimeout(() => {
+        this.userData.logout();
+      }, 1000);
+    }*/
   }
 
 }
