cbbeab55b1e4c10eb8215c5d39ae0eeffb6b4757
Kevin Noll <kevinnoll90@gmail.com>
2017-02-27 14:56:45 +0100

    added rotatable d3 positions

--- src/pages/map-overview/map-overview.ts
+++ src/pages/map-overview/map-overview.ts
@@ -15,8 +15,53 @@ import * as d3 from 'd3';
 export class MapOverviewPage {
   private title: String;
   private option: String;
+  private selOuterGroup: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  createSVG () {
+    let d3sel = d3.select("#d3");
+
+    let d3svg = d3sel.append("svg")
+                     .attr("width", 1024)
+                     .attr("height", 1024);
+                     debugger;
+    this.selOuterGroup = d3svg.append("g").classed("outerGroup", true);
+  }
+
+  appendBackgroundImage () {
+    this.selOuterGroup.append("svg:image")
+                     .attr("xlink:href", "assets/img/de_dust2.jpg")                     
+                     .attr("width", 1024)
+                     .attr("height", 1024);
+  }
+
+  appendDataSpots() {
+    d3.json("assets/data/dust2_spots.json", (err, data) => {
+      var d3spots = this.selOuterGroup.selectAll("g")
+         .data(data.spots)
+         .enter();
+      let selGroup = d3spots.append("g")
+            .attr("transform", function(d) { return "translate("+d.x + "," + d.y+")"; })
+      selGroup.append("circle")
+          .attr("cx", "25")
+          .attr("cy", "25")
+          .attr("r", "25")
+          .classed("rect", true);
+      selGroup.append("circle")
+          .attr("transform", "translate(15,15)")
+          .attr("cx", function(d) { return 10 })
+          .attr("cy", function(d) { return 10 })
+          .attr("r", 10)
+          .style("fill", "blue");
+     
+      selGroup.append("path")
+          .attr("d", d3.symbol()
+            .size(function(d) { return 200; } )
+            .type(function(d) { return d3.symbolTriangle; } ))
+          .attr("transform", "translate(25,44)");//function(d) { return "translate(" + 10 + "," + (10 + 20) + ")"; });
+    });
+  }
 
   ionViewDidLoad() {
     this.title = this.navParams.get("map").title;
@@ -24,40 +69,9 @@ export class MapOverviewPage {
 
     console.log('ionViewDidLoad MapOverviewPage');
 
-    var d3sel = d3.select("#d3");
-
-    var d3svg = d3sel.append("svg")
-                     .attr("width", 1024)
-                     .attr("height", 1024);
-
-    var d3g = d3svg.append("g");
-                  
-    var d3img = d3g.append("svg:image")
-                     .attr("xlink:href", "assets/img/de_dust2.jpg")                     
-                     .attr("width", 1024)
-                     .attr("height", 1024);
-
-    d3.json("assets/data/dust2_spots.json", function(err, data) {
-      var d3spots = d3g.selectAll("g")
-         .data(data.spots)
-         .enter();
-      let selGroup = d3spots.append("g")
-            .attr("transform", function(d) { return "translate("+d.x + "," + d.y+")"; })
-            .attr("width", "20")
-            .attr("height", "20");
-      selGroup.append("circle")
-          .attr("cx", function(d) { return 10 })
-          .attr("cy", function(d) { return 10 })
-          .attr("r", 10)
-          .style("fill", "blue");
-        selGroup.append("rect").classed("test", true)
-     
-      /*selGroup.append("path")
-          .attr("d", d3.symbol()
-            .size(function(d) { return 200; } )
-            .type(function(d) { return d3.symbolTriangle; } ))
-          .attr("transform", function(d) { return "translate(" + 10 + "," + (10 + 20) + ")"; });
-    */});
+    this.createSVG();
+    this.appendBackgroundImage();
+    this.appendDataSpots();
   }
 
 }
