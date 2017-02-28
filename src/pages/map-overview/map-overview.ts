e68cc1b00e9179254de763eca6e2cd9633c08eee
Kevin Noll <kevinnoll90@gmail.com>
2017-02-28 13:11:19 +0100

    map scales with browser window

--- src/pages/map-overview/map-overview.ts
+++ src/pages/map-overview/map-overview.ts
@@ -15,52 +15,101 @@ import * as d3 from 'd3';
 export class MapOverviewPage {
   private title: String;
   private option: String;
-  private selOuterGroup: any;
+  private height: Number;
+  private width: Number;
+  private maxWidth: Number;
+  private maxHeight: Number;
+  private selBackgroundImage: any;
+  private selMap: any;
+  private selSvg: any;
+  private d3sel: any;
+  private xScale: any;
+  private yScale: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.maxHeight = 1024;
+    this.maxWidth = 1024;
+  }
 
   createSVG () {
-    let d3sel = d3.select("#d3");
+    this.d3sel = d3.select("#d3");
 
-    let d3svg = d3sel.append("svg")
-                     .attr("width", 1024)
-                     .attr("height", 1024);
-                     debugger;
-    this.selOuterGroup = d3svg.append("g").classed("outerGroup", true);
+    this.selSvg = this.d3sel.append("svg")
+                     .attr("width", this.maxWidth)
+                     .attr("height", this.maxHeight);
+    this.selMap = this.selSvg.append("g")
+          .classed("map", true);
   }
 
   appendBackgroundImage () {
-    this.selOuterGroup.append("svg:image")
-                     .attr("xlink:href", "assets/img/de_dust2.jpg")                     
+    this.selBackgroundImage = this.selMap.append("svg:image")
+                     .attr("xlink:href", "assets/img/de_dust2.png")                     
                      .attr("width", 1024)
                      .attr("height", 1024);
   }
 
   appendDataSpots() {
-    d3.json("assets/data/dust2_spots.json", (err, data) => {
-      var d3spots = this.selOuterGroup.selectAll("g")
+    d3.json("assets/data/de_dust2.json", (err, data) => {
+      let d3spots = this.selMap.selectAll(".spot")
          .data(data.spots)
          .enter();
+
       let selGroup = d3spots.append("g")
-            .attr("transform", function(d) { return "translate("+d.x + "," + d.y+")"; })
+            .classed("spot", true)
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ") rotate(" + d.angle + " 25 25)"; })
       selGroup.append("circle")
-          .attr("cx", "25")
-          .attr("cy", "25")
-          .attr("r", "25")
-          .classed("rect", true);
+          .attr("cx", 25)
+          .attr("cy", 25)
+          .attr("r", 25)
+          .classed("turn-indicator", true);
       selGroup.append("circle")
           .attr("transform", "translate(15,15)")
-          .attr("cx", function(d) { return 10 })
-          .attr("cy", function(d) { return 10 })
-          .attr("r", 10)
-          .style("fill", "blue");
-     
+          .attr("cx", 10)
+          .attr("cy", 10)
+          .attr("r", 4)
+          .classed("player", true);
       selGroup.append("path")
           .attr("d", d3.symbol()
             .size(function(d) { return 200; } )
             .type(function(d) { return d3.symbolTriangle; } ))
-          .attr("transform", "translate(25,44)");//function(d) { return "translate(" + 10 + "," + (10 + 20) + ")"; });
+          .attr("transform", "translate(25,40)")
+          .classed("player-view", true);
     });
+  }
+
+  createScale() {
+        this.xScale = d3.scaleLinear().range([0,this.maxWidth]).domain([0,this.maxWidth]),
+        this.yScale = d3.scaleLinear().range([0,this.maxHeight]).domain([0,this.maxHeight]);
+
+        this.selMap.append("g")
+            .classed("xAxis", true)
+            .attr("transform", "translate(0, " + this.maxWidth + ")")
+            .call(d3.axisBottom(this.xScale)),
+        
+        this.selMap.append("g")            
+            .classed("yAxis", true)
+            .attr("transform", "translate(" + this.maxHeight + ", 0)")
+            .call(d3.axisLeft(this.yScale));
+  }
+  
+  render() {
+    //let minWidth = window.innerWidth;
+    let minWidth = this.d3sel.node().parentNode.offsetWidth;
+    this.width = minWidth < this.maxWidth ? minWidth : this.maxWidth;
+    this.height = this.width; // for 1:1 aspect ratio
+
+    // updates scales
+    this.xScale.range([0, this.width]);
+    this.yScale.range([this.height],0);
+
+    // update svg element
+    this.selSvg
+        .attr("width", this.width)
+        .attr("height", this.height);
+
+    this.selBackgroundImage
+        .attr("width", this.width)
+        .attr("height", this.height);
   }
 
   ionViewDidLoad() {
@@ -71,7 +120,11 @@ export class MapOverviewPage {
 
     this.createSVG();
     this.appendBackgroundImage();
-    this.appendDataSpots();
+    this.createScale();
+    this.render();
+    //this.appendDataSpots();
+
+    window.addEventListener('resize', this.render.bind(this));
   }
 
 }
