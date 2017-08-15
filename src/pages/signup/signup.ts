ace0f8f61dd9ed71947ba95116b1fa2708cef8fe
Kevin Noll <kevinnoll90@gmail.com>
2017-08-16 00:07:50 +0200

    added signup page

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { 
+  IonicPage, 
+  NavController, 
+  LoadingController, 
+  Loading, 
+  AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { WelcomePage } from '../welcome/welcome';
+import { EmailValidator } from '../../validators/email';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+  public signupForm:FormGroup;
+  public loading:Loading;
+
+  constructor(public nav: NavController, 
+    public authProvider: AuthServiceProvider, 
+    public formBuilder: FormBuilder, 
+    public loadingCtrl: LoadingController, 
+    public alertCtrl: AlertController) {
+
+    this.signupForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
+
+  /**
+   * If the form is valid it will call the authProvider service to sign the user up password displaying a loading
+   *  component while the user waits.
+   *
+   * If the form is invalid it will just log the form value, feel free to handle that as you like.
+   */
+  signupUser(){
+    if (!this.signupForm.valid){
+      console.log(this.signupForm.value);
+    } else {
+      this.authProvider.signupUser(this.signupForm.value.email, this.signupForm.value.password)
+      .then(() => {
+        this.nav.setRoot(WelcomePage);
+      }, (error) => {
+        this.loading.dismiss().then( () => {
+          var errorMessage: string = error.message;
+            let alert = this.alertCtrl.create({
+              message: errorMessage,
+              buttons: [
+                {
+                  text: "Ok",
+                  role: 'cancel'
+                }
+              ]
+            });
+          alert.present();
+        });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+}
