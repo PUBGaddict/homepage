c68ed02133b28fed31181aef30544e5498f5d987
Kevin Noll <kevinnoll90@gmail.com>
2017-08-16 00:28:06 +0200

    added login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,23 +1,70 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { 
+  IonicPage, 
+  NavController, 
+  LoadingController, 
+  Loading, 
+  AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { EmailValidator } from '../../validators/email';
+import { WelcomePage } from '../welcome/welcome';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+@IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public loginForm:FormGroup;
+  public loading:Loading;
+
+  constructor(public navCtrl: NavController, public authData: AuthServiceProvider, 
+    public formBuilder: FormBuilder, public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+
+      this.loginForm = formBuilder.group({
+        email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+        password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+      });
+    }
+
+    loginUser(){
+      if (!this.loginForm.valid){
+        console.log(this.loginForm.value);
+      } else {
+        this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+        .then( authData => {
+          this.navCtrl.setRoot(WelcomePage);
+        }, error => {
+          this.loading.dismiss().then( () => {
+            let alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [
+                {
+                  text: "Ok",
+                  role: 'cancel'
+                }
+              ]
+            });
+            alert.present();
+          });
+        });
+
+        this.loading = this.loadingCtrl.create({
+          dismissOnPageChange: true,
+        });
+        this.loading.present();
+      }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  goToResetPassword(){
+    this.navCtrl.push('ResetPasswordPage');
+  }
+
+  createAccount(){
+    this.navCtrl.push('SignupPage');
   }
 
 }
