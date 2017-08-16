b926b9b33bbd86939c23e93248c9c659111c0d40
Kevin Noll <kevinnoll90@gmail.com>
2017-08-16 14:59:15 +0200

    added validator for username

---
+++ src/validators/username.ts
@@ -0,0 +1,21 @@
+import { FormControl } from '@angular/forms';
+
+export class UsernameValidator {
+  
+  static isValid(control: FormControl){
+
+    let value = control.value;    
+    if (!value){
+      return { "username is empty" : true };
+    }
+
+    if (value.replace(new RegExp(' ', 'g'), '').length < 3) {
+			return { "username is too short" : true };
+    }
+    
+    return {
+      "invalidEmail": true
+    };
+    
+  }
+}
